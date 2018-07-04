98f6acd5bceb2a74969fdfa24f788343e4730d80
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-07-04 14:36:59 +0700

    reset_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   scope "(:locale)", :locale => /en|vi/ do
     root "static_pages#home"
     get "/signup", to: "users#new"
